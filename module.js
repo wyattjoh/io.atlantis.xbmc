1d67cb7abff4735fb9c24729e38ae3e7b8e717ae
Wyatt Johnson <wyattjoh@gmail.com>
2014-11-12 00:42:01 -0700

    Added some more plugin states

--- module.js
+++ module.js
@@ -1,7 +1,7 @@
 (function() {
     var app = angular.module("io.atlantis.xbmc", [ 'io.atlantis' ]);
 
-    var atlantisTile = function($ocLazyLoad, ColorService, ResourceService, $interval, dateFilter) {
+    var atlantisTile = function($http, ColorService, ResourceService, $interval) {
         return {
             restrict: 'E',
             templateUrl: '/assets/plugins/io.atlantis.xbmc/module.tpl.html',
@@ -10,11 +10,68 @@
                     scope.config = config;
                 });
 
-                scope.getRandomColor = ColorService.getRandomColor;
+                var defaultPluginStyle = {
+                    'background': '',
+                    'display': 'none'
+                };
 
-                timeoutId = $interval(function() {
-                    scope.time = dateFilter(new Date(), 'M/d/yy h:mm:ss a');
-                }, 1000);
+                scope.pluginClasses = [];
+                scope.pluginStyle = {
+                    'background': '',
+                    'display': 'none'
+                };
+
+                var updateFromPlayingItem = function(item) {
+                    scope.currentlyPlaying = item;
+
+                    scope.pluginStyle['display'] = 'block';
+
+                    if (item["art"]["tvshow.banner"]) {
+                        scope.pluginStyle['background'] = "url('http://xbmc.atlantis.io/image/" + encodeURIComponent(item["art"]["tvshow.banner"]) + "')";
+                        scope.pluginClasses = [];
+                    } else {
+                        scope.pluginStyle['background'] = "url('http://xbmc.atlantis.io/image/" + encodeURIComponent(item.art.fanart) + "')";
+                        scope.pluginClasses = ["movie-animated"];
+                    }
+                };
+
+                var updateFromXbmc = function() {
+                    $http.post("http://xbmc.atlantis.io/jsonrpc", {"jsonrpc": "2.0", "method": "Player.GetActivePlayers", "id": 1})
+                        .success(function(response) {
+                            if (response.result.length) {
+
+                                var video_properties = {
+                                    "jsonrpc": "2.0",
+                                    "method": "Player.GetItem",
+                                    "params": { "properties": [ "showtitle", "fanart", "art" ], "playerid": 1 },
+                                    "id": "VideoGetItem"
+                                };
+
+                                $http.post("http://xbmc.atlantis.io/jsonrpc", video_properties)
+                                    .success(function(data) {
+                                        console.log(data);
+
+                                        if (data.result) {
+                                            // Update the UI
+                                            updateFromPlayingItem(data.result.item);
+                                        }
+                                    });
+                            } else {
+                                // There is nothing playing, clear the ui
+                                for (var key in defaultPluginStyle) {
+                                    scope.pluginStyle[key] = defaultPluginStyle[key];
+                                }
+
+                                scope.currentlyPlaying = null;
+                            }
+                        });
+                };
+
+                updateFromXbmc();
+
+                $interval(updateFromXbmc, 2000);
+
+                scope.getRandomColor = ColorService.getRandomColor;
             }
         }
     };
